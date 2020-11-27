e391010f50a5a83dc9d4516baf9ebe632f475a35
prakruthin <49477322+prakruthin@users.noreply.github.com>
2020-11-27 09:09:19 +0530

    Update food.c

--- Food_order/3_Implementation/src/food.c
+++ Food_order/3_Implementation/src/food.c
@@ -1,36 +1,4 @@
 #include "food.h"
-//#include <termios.h>
-#include <unistd.h>
-#include <stdio.h>
-
-/* reads from keypress, doesn't echo */
-int getch(void)
-{
-    struct termios oldattr, newattr;
-    int ch;
-    tcgetattr( STDIN_FILENO, &oldattr );
-    newattr = oldattr;
-    newattr.c_lflag &= ~( ICANON | ECHO );
-    tcsetattr( STDIN_FILENO, TCSANOW, &newattr );
-    ch = getchar();
-    tcsetattr( STDIN_FILENO, TCSANOW, &oldattr );
-    return ch;
-}
-
-/* reads from keypress, echoes */
-int getche(void)
-{
-    struct termios oldattr, newattr;
-    int ch;
-    tcgetattr( STDIN_FILENO, &oldattr );
-    newattr = oldattr;
-    newattr.c_lflag &= ~( ICANON );
-    tcsetattr( STDIN_FILENO, TCSANOW, &newattr );
-    ch = getchar();
-    tcsetattr( STDIN_FILENO, TCSANOW, &oldattr );
-    return ch;
-}
-
 
 struct menu
 {
@@ -513,7 +481,7 @@ void Password(void)
 		ch=getch();
 		if(ch!=13 && ch!=8)
 		{
-			printf("*");
+			putch('*');
 			pass[i]=ch;
 			i++;
 		}
